a691256d9b545079de1a06578c77388249091ff7
cjlangan <connorjameslangan@gmail.com>
2024-11-24 21:17:11 -0600

    Added colours to script, and input

--- ylct.sh
+++ ylct.sh
@@ -1,18 +1,18 @@
 #!/bin/bash 
 
-YOUTUBER="Ludwig"
-API_KEY=${YT_API_KEY}
+YOUTUBER="$1" # reads in first argument
+API_KEY=${YT_API_KEY} # reads environment variable
 PAGE_TOKEN=""
 POLL_INTERVAL=0
 
 #Get the channel ID of the Youtuber
-CHANNEL_ID=$(curl "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${YOUTUBER}&key=${API_KEY}&type=channel" | jq -r '.items[0].id.channelId')
-echo "Obtained Channel ID of $YOUTUBER"
+CHANNEL_ID=$(curl -s "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${YOUTUBER}&key=${API_KEY}&type=channel" | jq -r '.items[0].id.channelId')
+echo "Obtained Channel ID of $YOUTUBER: $CHANNEL_ID"
 
 # Get the live video id 
-VIDEO_ID=$(curl "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&eventType=live&type=video&key=${API_KEY}" | jq -r '.items[0].id.videoId')
+VIDEO_ID=$(curl -s "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&eventType=live&type=video&key=${API_KEY}" | jq -r '.items[0].id.videoId')
 
-if [ "$VIDEO_ID" == "null"]; then
+if [ "$VIDEO_ID" == "null" ]; then
     echo "$YOUTUBER is not live."
     exit 1
 fi 
@@ -20,7 +20,7 @@ fi
 echo "Obtained Video id of livestream"
 
 # Get the live chat id
-LIVE_CHAT_ID=$(curl "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails,snippet&id=${VIDEO_ID}&key=${API_KEY}" | jq -r '.items[0].liveStreamingDetails.activeLiveChatId')
+LIVE_CHAT_ID=$(curl -s "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails,snippet&id=${VIDEO_ID}&key=${API_KEY}" | jq -r '.items[0].liveStreamingDetails.activeLiveChatId')
 echo "Obtained live chat ID"
 
 # Continually grab chat messages
@@ -29,8 +29,19 @@ while true; do
     # Get messages
     RESPONSE=$(curl -s "https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${LIVE_CHAT_ID}&part=snippet,authorDetails&maxResults=2000&pageToken=${PAGE_TOKEN}&key=${API_KEY}")
 
-    # Print the messages
-    echo "$RESPONSE" | jq -r '.items[] | "\(.authorDetails.displayName): \(.snippet.displayMessage)"'
+    # Print the messages 
+    # Mods get blue names, Members get green names
+    echo "$RESPONSE" | jq -r '
+        .items[] | 
+        (
+            if .authorDetails.isChatModerator == true then
+                "\033[0;34m\(.authorDetails.displayName)\033[0m: \(.snippet.displayMessage)"
+            elif .authorDetails.isChatSponsor == true then
+                "\033[0;32m\(.authorDetails.displayName)\033[0m: \(.snippet.displayMessage)"
+            else
+                "\(.authorDetails.displayName): \(.snippet.displayMessage)"
+            end
+        )'
 
     # Grab the next page token and polling interval
     PAGE_TOKEN=$(echo "$RESPONSE" | jq -r '.nextPageToken')
